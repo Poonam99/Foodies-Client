f5f015b849ba1aca04ddb3ea2ca4efdef4f3d8a8
Poonam99 <poonamtabassum9909@gmail.com>
2022-11-10 03:54:33 +0600

    ans 2

--- src/Component/Pages/Blogs/Blogs.js
+++ src/Component/Pages/Blogs/Blogs.js
@@ -13,6 +13,21 @@ const Blogs = () => {
 
                     There are five practical differences between SQL and NoSQL:Language,Scalability,Structure,Properties,Support and communities</p>
             </div>
+            <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50">
+
+                <div className="mt-6 mb-2">
+
+                    <h2 className="text-xl font-semibold tracking-wide">What is the difference between javascript and NodeJS?</h2>
+                </div>
+                <p className="dark:text-gray-100">JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed.
+                    Node. js,  is an interpreter or execution environment for the JavaScript programming language.
+                    *Javascript is a programming language that is used for writing scripts on the website.
+                    *NodeJS is a Javascript runtime environment.
+                    *Javascript can only be run in the browsers.
+                    *We can run Javascript outside the browser with the help of NodeJS.
+                    *It is basically used on the client-side.
+                    *It is mostly used on the server-side.</p>
+            </div>
         </div>
     );
 };
