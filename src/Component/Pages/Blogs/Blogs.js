24a53c684a6029a14515a0c36ba05ad027ca88b1
Poonam99 <poonamtabassum9909@gmail.com>
2022-11-10 04:02:43 +0600

    added4th

--- src/Component/Pages/Blogs/Blogs.js
+++ src/Component/Pages/Blogs/Blogs.js
@@ -17,6 +17,16 @@ const Blogs = () => {
 
                 <div className="mt-6 mb-2">
 
+                    <h2 className="text-xl font-semibold tracking-wide">What is JWT, and how does it work?</h2>
+                </div>
+                <p className="dark:text-gray-100">JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object.
+                    It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider.
+                    Each JWT contains encoded JSON objects, including a set of claims.</p>
+            </div>
+            <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50">
+
+                <div className="mt-6 mb-2">
+
                     <h2 className="text-xl font-semibold tracking-wide">What is the difference between javascript and NodeJS?</h2>
                 </div>
                 <p className="dark:text-gray-100">JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed.
@@ -35,6 +45,14 @@ const Blogs = () => {
                     <h2 className="text-xl font-semibold tracking-wide">How does NodeJS handle multiple requests at the same time?</h2>
                 </div>
                 <p className="dark:text-gray-100">NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them. Multiple clients make multiple requests to the NodeJS server. NodeJS receives these requests and places them into the EventQueue .</p>
+            </div>
+            <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50">
+
+                <div className="mt-6 mb-2">
+
+                    <h2 className="text-xl font-semibold tracking-wide"></h2>
+                </div>
+                <p className="dark:text-gray-100"></p>
             </div>
         </div>
     );
