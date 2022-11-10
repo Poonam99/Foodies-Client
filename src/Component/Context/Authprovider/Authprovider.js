eeceb9374a401854e35889c463d1d352afb155c3
Poonam99 <poonamtabassum9909@gmail.com>
2022-11-10 16:21:02 +0600

    AuthContext Page Done

--- src/Component/Context/Authprovider/Authprovider.js
+++ src/Component/Context/Authprovider/Authprovider.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
+import app from '../../firebase/firebase.config';
 
-const Authprovider = () => {
+const auth = getAuth(app)
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = (email, pass) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, pass);
+    }
+
+    const updateUserProfile = (profile) => {
+        return updateProfile(auth.currentUser, profile);
+    }
+
+    const login = (email, pass) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, pass);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        localStorage.removeItem('animator-user-token');
+        return signOut(auth);
+    }
+
+    const googleSignIn = () => {
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false)
+        });
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+    const authInfo = {
+        user,
+        loading,
+        setLoading,
+        createUser,
+        login,
+        logOut,
+        googleSignIn,
+        updateUserProfile
+    }
     return (
-        <div>
-
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
-export default Authprovider;
+export default AuthProvider;
